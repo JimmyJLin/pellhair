fdf9a2568edbfd030567d1f1f8a7aabd4ecfe0fd
Jimmy Lin <usmcjimmy@gmail.com>
2016-06-13 16:15:39 -0400

    change color on hover for header nav bar

--- public/js/script.js
+++ public/js/script.js
@@ -4,11 +4,79 @@ $(function(){
   // Yelp Star Rating
   $('.ui.rating').rating()
 
+  // Social media on hover change color
+  $('#socialfacebook').hover(
+    function(){
+      $(this).removeClass("grey").addClass("blue")
+    },
+    function(){
+      $(this).removeClass("blue").addClass("grey")
+    }
+  )
 
+  $('#socialfoursquare').hover(
+    function(){
+      $(this).removeClass("grey").addClass("pink")
+    },
+    function(){
+      $(this).removeClass("pink").addClass("grey")
+    }
+  )
 
+  $('#socialwechat').hover(
+    function(){
+      $(this).removeClass("grey").addClass("green")
+    },
+    function(){
+      $(this).removeClass("green").addClass("grey")
+    }
+  )
 
+  $('#socialinstagram').hover(
+    function(){
+      $(this).removeClass("grey").addClass("orange")
+    },
+    function(){
+      $(this).removeClass("orange").addClass("grey")
+    }
+  )
 
+  $('#socialyelp').hover(
+    function(){
+      $(this).removeClass("grey").addClass("red")
+    },
+    function(){
+      $(this).removeClass("red").addClass("grey")
+    }
+  )
 
+  // Menu button on hover
+  $('#buttonhome').hover(
+    function(){
+      $(this).css({"background-color":"#666", "color":"white"})
+    },
+    function(){
+      $(this).css({"background-color":"white", "color":"black"})
+    }
+  )
+
+  $('#buttonservice').hover(
+    function(){
+      $(this).css({"background-color":"#666", "color":"white"})
+    },
+    function(){
+      $(this).css({"background-color":"white", "color":"black"})
+    }
+  )
+
+  $('#buttoncontact').hover(
+    function(){
+      $(this).css({"background-color":"#666", "color":"white"})
+    },
+    function(){
+      $(this).css({"background-color":"white", "color":"black"})
+    }
+  )
 
 
 
