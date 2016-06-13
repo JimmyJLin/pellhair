96c7d3a70f9d5c9b914911b104117c13533577e2
Jimmy Lin <usmcjimmy@gmail.com>
2016-06-13 12:45:26 -0400

    setup header, footer and basic menu page with semantic-ui

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ const logger = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
 // const serviceRoutes = require(path.join(__dirname, 'routes/services'));
-// const contactRoutes = require(path.join(__dirname, 'routes/contact'));
 
 /* app setting */
 const port = process.env.PORT || 3000;
 const app = express();
 const server = app.listen(port);
 const request = require('request');
-
 
 // parse incoming forms
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,9 +25,7 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 /* Routes */
-// Landing Page
 // app.use('/services', serviceRoutes)
-// app.use('/contact', contactRoutes)
 
 app.get('/services', function(req, res){
   res.render('pages/services')
